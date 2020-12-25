d556d39f5e227e7fdec272079bc0b02b5c58cb43
marcelom97 <marcelomollaj@gmail.com>
2020-12-25 22:12:42 +0200

    refactor: added userRouter and connectDB

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const { red } = require('colors');
+const connectDB = require('./config/connectDB');
 const errorHandler = require('./middlewares/errorHandler');
+const userRouter = require('./routes/userRouter');
 
 dotenv.config({ path: './config/config.env' });
 
 const app = (module.exports = express());
+
+connectDB();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,4 +23,11 @@ app.get('/ping', (req, res, next) => {
   });
 });
 
+app.use('/api/v1/users', userRouter);
+
 app.use(errorHandler);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.log(red(`Error: ${err.message}`));
+});

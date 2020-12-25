2b723b99b311aceb9e4419274d0230e39a4b7595
marcelom97 <marcelomollaj@gmail.com>
2020-12-25 21:29:43 +0200

    feat: basic express server initialization

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+dotenv.config({ path: './config/config.env' });
+
+const app = (module.exports = express());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.get('/ping', (req, res, next) => {
+  res.status(200).json({
+    message: 'pong',
+  });
+});
---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Express Server started on http://localhost:${PORT}`);
+});

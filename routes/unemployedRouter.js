478f6a9616b6a8da498c48f82d8d58aff970a77b
marcelom97 <marcelomollaj@gmail.com>
2020-12-26 00:23:30 +0200

    feat: added role auth to unemployedRouter

--- routes/unemployedRouter.js
+++ routes/unemployedRouter.js
@@ -12,9 +12,18 @@ const Unemployed = require('../models/Unemployed');
 
 const advancedResults = require('../middlewares/advancedResults');
 
+const { protectRoute, authorizedUser } = require('../middlewares/authHandler');
+
 const router = express.Router();
 
-router.route('/').get(advancedResults(Unemployed), getAllUnemployed).post(createNewUnemployed);
-router.route('/:id').get(getUnemployedById).put(updateUnemployedById).delete(deleteUnemployedById);
+router
+  .route('/')
+  .get(protectRoute, authorizedUser('admin'), advancedResults(Unemployed), getAllUnemployed)
+  .post(protectRoute, createNewUnemployed);
+router
+  .route('/:id')
+  .get(getUnemployedById)
+  .put(protectRoute, authorizedUser('admin'), updateUnemployedById)
+  .delete(protectRoute, authorizedUser('admin'), deleteUnemployedById);
 
 module.exports = router;

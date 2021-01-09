e7bf553c375d30dc15b69190f94275f43062ebba
marcelom97 <marcelomollaj@gmail.com>
2021-01-09 16:52:39 +0200

    refactor: added env variable FRONTEND_URL for cors

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 );
